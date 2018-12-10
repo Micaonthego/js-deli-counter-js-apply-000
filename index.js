fce6dae9bf892b1e2e0552b59c3864c3fd12df9b
Micaela Rivera <micaonthego@gmail.com>
2018-12-10 23:53:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name) 
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) 
+  return `Currently serving ${katzDeliLine.shift(name)}.`
+  else (katzDeliLine.length = 0)
+  return "There is nobody waiting to be served!"
+}
+
+function currentLine (katzDeliLine) {
+  if (katsDeliLine.length > 0)
+  return `The line is currently: ${()}`
+  else (katzDeliLine.length = 0)
+  return "The line is currently empty."
+}
